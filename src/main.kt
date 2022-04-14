3b64253e5723cc99a1b01a554ce0cd41b52221d0
gathangamaryann <maryanngathanga@gmail.com>
2022-04-14 10:20:45 +0300

    changed int to double

--- src/main.kt
+++ src/main.kt
@@ -27,56 +27,77 @@ the characters in even indices. For example given “banana” it will return
 “bnn”(2 points)*/
 
 
-
-
-
 fun main(){
-    var money=CurrentAccount(5649,"mymoney",3569.3)
-    var veggies=Product("cabbage",3.4,60,"groceries")
-    var cotton = Product("pinky",2.3,300,"towel")
-    var pen = Product("bluepen",1.3,40,"others")
+    var myAccount=CurrentAccount("05574634","airbank",10000.0)
+    myAccount.deposit(5000.0)
+    myAccount.withdraw(500.5)
+    myAccount.details()
 
+    var mySavingsAccount=SavingsAccount("000475663","marybank",50000.5,2)
+    mySavingsAccount.deposit(3000.5)
+    mySavingsAccount.withdraw(1000.5)
+    println( mySavingsAccount.withdrawals)
+    mySavingsAccount.details()
 
+    var a=Product("handkerchif",12.0,10.0,"hygiene")
+    var b=Product("tissue",10.0,30.5,"hygiene")
+    var c=Product("pen",1.5,10.5,"other")
+    var d=Product("cabbage",3.5,70.0,"groceries")
+    assignProducts(a)
+    assignProducts(b)
+    assignProducts(c)
+    assignProducts(d)
 
-
+    evenIndicesChar("banana")
+    evenIndicesChar("maryannn")
 
 
 }
- class CurrentAccount(var accountnumber:Int,var accountname:String, var balance:Double){
-     fun deposit(amount:Double){
-         var depots=500
-         println(balance+depots)
+open class CurrentAccount(var accountNumber:String,var accountName:String,var balance:Double){
+    fun deposit(amount:Double){
+        balance+=amount
+        println(balance)
+    }
+    open fun withdraw(amount: Double){
+        balance-=amount
+        println(balance)
+    }
+    fun details(){
+        println("Account number $accountNumber with balance $balance is operated by $accountName")
+    }
 
-     }
-     fun withdrawal(amount: Double){
-
-     }
-     fun details(){
-
-     }
- }
-
-class SavingsAccount(var accountnumber: Int,var accountname: String,var balance: Double){
-    fun  withdrawal(withdrawal:Int){
-
+}
+class SavingsAccount(accountNumber: String,accountName: String,balance: Double, var withdrawals:Int):CurrentAccount(accountNumber,accountName,balance){
+    override fun withdraw(amount: Double) {
+        if (withdrawals<4){
+            balance-=amount
+        }
+        println(balance)
+        withdrawals++
     }
 }
-data class Product(var name:String,var weight:Double,var price:Int,var category:String){
-    fun clothes(){
+data class Product(var name:String, var weight:Double,var price:Double,var category:String)
+fun assignProducts(product:Product){
 
+    var groceriesLists= mutableListOf<Product>()
+    var hygieneLists= mutableListOf<Product>()
+    var otherLists= mutableListOf<Product>()
+    when(product.category){
+        "groceries"-> groceriesLists.add(product)
+        "hygiene"->hygieneLists.add(product)
+        "other"->otherLists.add(product)
     }
-    fun food(){
+    println( listOf(product))
 
-    }
-    fun stationery(){
-
-    }
 }
+fun evenIndicesChar(word:String):String{
+    var newWord=""
+    word.forEachIndexed { index, c ->
+        if (index%2==0){
+            newWord+=c
+        }
+    }
+    println(newWord)
+    return newWord
 
-fun fruit():String{
-
-    var type = "banana"
-
-    println(fruit())
-    return type
 }
